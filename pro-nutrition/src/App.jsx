246a89ba259c3b416b7d33828bf54d2cc264a7ee
Sarvesh <sarveshjaiofficial@gmail.com>
2024-01-23 12:46:42 +0530

    added

--- pro-nutrition/src/App.jsx
+++ pro-nutrition/src/App.jsx
@@ -1,31 +1,27 @@
+import "./App.css";
+import Search from "./components/Search";
+import FoodBox from "./components/FoodBox";
+import FoodData from "../resources/FoodData.js";
+import { useState } from "react";
 
-import './App.css'
-import Search from './assets/components/Search'
-import FoodBox from './assets/components/FoodBox'
-import foodData from "../resources/FoodData.js"
-import { useState } from 'react'
 function App() {
-
   const [search, setSearch] = useState("");
-  let filteredArray =foodData.filter((e) => {
-        return   e.name.toLowerCase().includes(search.toLowerCase())  
-      })
+
+  const filteredFoodData = FoodData.filter((e) =>
+    e.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <>
-      <h1>PRO NUTRITION 🎯</h1>
-      <br />
-      <br />
+      <h1>Pro Nutrition</h1>
       <Search setSearch={setSearch} />
-      <br />
-      <br />
-
-      {filteredArray.length==0?<p>No results found</p> : filteredArray.map((e,i)=>{
-        return <FoodBox data={e} index={i} key={ i} />
-      })}
-     
-
+      {filteredFoodData.length > 0 ? (
+        filteredFoodData.map((e, i) => <FoodBox key={i} data={e} index={i} />)
+      ) : (
+        <h1>No matching items found.</h1>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- pro-nutrition/src/main.jsx
+++ pro-nutrition/src/main.jsx
@@ -1,10 +1,10 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import Test from "./assets/components/Test.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <>
-    <Test />
-  </>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
